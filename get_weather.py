61fa1cfc8abb76dd8937d9e9b684af5da7869139
Duy Tran <anhduytran5454@gmail.com>
2025-08-07 14:08:10 +0700

    initial commit

---
+++ get_weather.py
@@ -0,0 +1,11 @@
+import requests
+import json
+from datetime import datetime
+from utils.convert_time import convert_gmt_to_unix
+
+# Define variables
+API_KEY = '1fd9185f943369cf7626513f4208bed6'
+API_URL = 'http://api.openweathermap.org/data/2.5/weather'
+lat_hanoi = 21.0285
+long_hanoi = 105.8542
+
