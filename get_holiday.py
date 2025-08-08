d9866913c7ed97f24bb814a3bb59b97198d2cfa1
Duy Tran <anhduytran5454@gmail.com>
2025-08-08 14:36:32 +0700

    close project

--- get_holiday.py
+++ get_holiday.py
@@ -1,1 +1,26 @@
 import requests
+import pandas as pd
+
+url = 'https://api.11holidays.com/v1/holidays?country=VN&year={year}'
+year = ['2023', '2024', '2025', '2026', '2027', '2028', '2029', '2030', '2031', '2032', '2033']
+holiday = []
+response = requests.get(url)
+
+for years in year:
+    response = requests.get(url.format(year=years))
+    if response.status_code == 200:
+        holidays = response.json()
+        date = [holiday['date'] for holiday in holidays]
+        name = [holiday['name'] for holiday in holidays]
+        print(date, name)
+    else:
+        print(f"Failed to retrieve data: {response.status_code}")
+    holiday.extend(zip(date, name))
+
+holiday_df = pd.DataFrame(holiday, columns=['date', 'name'])
+holiday_df['date'] = pd.to_datetime(holiday_df['date'])
+holiday_df['year'] = holiday_df['date'].dt.year
+holiday_df['month'] = holiday_df['date'].dt.month
+holiday_df['day'] = holiday_df['date'].dt.day
+holiday_df['date'] = holiday_df['date'].dt.strftime('%Y-%m-%d')
+holiday_df.to_csv('holidays_vietnam.csv', index=False)
--- get_weather.py
+++ get_weather.py
@@ -1,11 +1,56 @@
 import requests
-import json
-from datetime import datetime
-from utils.convert_time import convert_gmt_to_unix
+import csv
 
-# Define variables
-API_KEY = '1fd9185f943369cf7626513f4208bed6'
-API_URL = 'http://api.openweathermap.org/data/2.5/weather'
-lat_hanoi = 21.0285
-long_hanoi = 105.8542
+province_list = [
+    'An Giang', 'Bắc Ninh', 'Cao Bằng', 'Cà Mau', 'Gia Lai',
+    'Hà Nội', 'Hà Tĩnh', 'Hòa Bình', 'Hưng Yên', 'Lai Châu',
+    'Lào Cai', 'Lâm Đồng', 'Lạng Sơn', 'Nghệ An', 'Ninh Bình',
+    'Ninh Thuận', 'Quảng Ngãi', 'Quảng Ninh', 'Quảng Trị',
+    'Sơn La', 'Thanh Hóa', 'Thành phố Cần Thơ', 'Thành phố Hải Phòng',
+    'Thành phố Hồ Chí Minh', 'Thái Nguyên', 'Tuyên Quang',
+    'Tây Ninh', 'Vĩnh Long', 'Điện Biên', 
+    'Đà Nẵng', 'Đắk Lắk',
+    'Đồng Nai', 'Đồng Tháp', 'Thừa Thiên Huế'
+]
+lat_list = [
+    10.267, 21.2335, 22.6657, 9.2354, 13.8828,
+    21.0285, 18.3428, 21.1911, 20.5485, 22.3862,
+    22.0955, 11.7712, 21.8537, 18.6731, 20.4113,
+    11.9032, 14.7352, 21.005, 17.1433,
+    21.3274, 19.8066, 9.6806, 20.9373,
+    10.7317, 21.8706, 22.3233,
+    10.9271, 10.1417, 21.386, 
+    15.7932, 12.8848,
+    11.3508, 10.4157, 16.4637
+]
+lon_list = [
+    105.1031, 106.137, 106.257, 105.4384, 108.61,
+    105.8542, 105.9057, 105.3987, 106.1966, 103.4586,
+    104.4208, 108.0548, 106.7615, 105.6923, 106.0195,
+    109.0926, 108.3884, 107.2925, 106.857,
+    103.9144, 105.7852, 105.811, 106.3147,
+    106.8303, 105.8414, 105.099,
+    106.2562, 106.2311, 103.023, 
+    108.1064, 108.6828,
+    106.8741, 105.9826, 107.5909
+]
 
+# Define date range
+start_date = "2023-01-01"
+end_date = "2025-08-07"
+
+# open CSV file for writing
+with open("weather3.csv", mode="w", newline="", encoding="utf-8") as file:
+    writer = csv.writer(file)
+    writer.writerow(["province", "date", "max_temp", "min_temp", "precipitation"])
+
+    for province, lat, lon in zip(province_list, lat_list, lon_list):
+        url = f"https://archive-api.open-meteo.com/v1/archive?latitude={lat}&longitude={lon}&start_date={start_date}&end_date={end_date}&daily=temperature_2m_max,temperature_2m_min,precipitation_sum&timezone=Asia%2FBangkok"
+        response = requests.get(url)
+        data = response.json()
+        for i in range(len(data['daily']['time'])):
+            date = data['daily']['time'][i]
+            temp_max = data['daily']['temperature_2m_max'][i]
+            temp_min = data['daily']['temperature_2m_min'][i]
+            precipitation = data['daily']['precipitation_sum'][i]
+            writer.writerow([province, date, temp_max, temp_min, precipitation])
